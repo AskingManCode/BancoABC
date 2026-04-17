7164dda3c13558a29a5e5f2ebf7f26689c4d0833
AskingMansOzGlobal <sebasjimearrieta@gmail.com>
2026-04-16 19:57:41 -0600

    Cambio Prueba

---
+++ Interfaz_Adm_Usr/Interfaz_Adm_Usr/Pages/Folder/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Interfaz_Adm_Usr.Pages.Folder
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
