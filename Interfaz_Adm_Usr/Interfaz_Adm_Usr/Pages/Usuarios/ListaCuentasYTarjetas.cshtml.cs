d13a32e49ea987ad4de11758d22fe6ddbd467939
AskingMansOzGlobal <sebasjimearrieta@gmail.com>
2026-04-19 17:09:25 -0600

    Cambios en MasterPage y en Usuarios (solo para evitar entradas falsas según el rol)
    Comentarios para evitar login en el desarrollo

--- Interfaz_Adm_Usr/Interfaz_Adm_Usr/Pages/Usuarios/ListaCuentasYTarjetas.cshtml.cs
+++ Interfaz_Adm_Usr/Interfaz_Adm_Usr/Pages/Usuarios/ListaCuentasYTarjetas.cshtml.cs
@@ -5,8 +5,25 @@ namespace Interfaz_Adm_Usr.Pages.Usuarios
 {
     public class ListaCuentasYTarjetasModel : PageModel
     {
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            // Verifica si hay sesión
+            var tipoUsuario = HttpContext.Session.GetString("TipoUsuario");
+
+            if (string.IsNullOrEmpty(tipoUsuario))
+            {
+                // No hay login, devolver al login
+                return RedirectToPage("/Index");
+            }
+
+            // Verificar que sea Usuario
+            if (tipoUsuario != "2")
+            {
+                // No hay login, devolver al login
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
--- Interfaz_Adm_Usr/Interfaz_Adm_Usr/Pages/Usuarios/MovimientosTarjetasCredito.cshtml.cs
+++ Interfaz_Adm_Usr/Interfaz_Adm_Usr/Pages/Usuarios/MovimientosTarjetasCredito.cshtml.cs
@@ -5,10 +5,25 @@ namespace Interfaz_Adm_Usr.Pages.Usuarios
 {
     public class MovimientosTarjetasCreditoModel : PageModel
     {
-        // string ID = HttpContext.Session.GetString("IdentificacionUsuario");
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            // Verifica si hay sesión
+            var tipoUsuario = HttpContext.Session.GetString("TipoUsuario");
 
+            if (string.IsNullOrEmpty(tipoUsuario))
+            {
+                // No hay login, devolver al login
+                return RedirectToPage("/Index");
+            }
+
+            // Verificar que sea Usuario
+            if (tipoUsuario != "2")
+            {
+                // No hay login, devolver al login
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
