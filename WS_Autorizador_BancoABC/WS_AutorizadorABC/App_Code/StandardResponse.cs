0cbb1de135d4d4cd239c9f7b92503f9ce69430d9
AskingMansOzGlobal <sebasjimearrieta@gmail.com>
2026-04-22 13:26:57 -0600

    StandardResponse

---
+++ WS_Autorizador_BancoABC/WS_AutorizadorABC/App_Code/StandardResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+[DataContract]
+public class StandardResponse<T>
+{
+    [DataMember]
+    public bool Resultado { get; set; }
+
+    [DataMember]
+    public string Mensaje { get; set; }
+
+    [DataMember]
+    public T Datos { get; set; }
+}
