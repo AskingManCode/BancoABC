7a21d774cfa4aec87ebab356e9a4a3348454676d
AskingMansOzGlobal <sebasjimearrieta@gmail.com>
2026-04-22 16:28:56 -0600

    Personas

---
+++ WS_Autorizador_BancoABC/CapaNegocio/Personas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[DataContract]
+public class Personas
+{
+    [DataMember]
+    public string Identificacion { get; set; }
+
+    [DataMember]
+    public string Nombre { get; set; }
+
+    [DataMember]
+    public string PrimerApellido { get; set; }
+
+    [DataMember]
+    public string SegundoApellido { get; set; }
+
+    [DataMember]
+    public string Correo { get; set; }
+}
