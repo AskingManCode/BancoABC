ea20577d9cd91d5a0fcbd206748519aa3e2d1f66
FranquitoCchar <acunacordobafranco@gmail.com>
2026-04-21 22:22:53 -0600

    Core bancario ambientado para USR2, USR3 y USR4.

--- CoreBancario_BancoABC/src/corebancario_bancoabc/LogicaCore.java
+++ CoreBancario_BancoABC/src/corebancario_bancoabc/LogicaCore.java
@@ -119,7 +119,69 @@ public class LogicaCore { // Inicia Clase
                     bitacora = new Bitacora(llaveValor, "OK");
                     bitacora.start();
                 }
+                
+            //-------------------------------------------------------------------------------------------
+            
+            } else if ("ObtenerCuentas".equals(llaveValor.get("TipoDeTransaccion"))) {
 
+                String identificacion = llaveValor.get("Identificacion");
+                List<Object[]> cuentas = sqlConnection.selectAll(
+                    "SELECT CUENTA_ID, CUENTA_MontoDisponible " +
+                    "FROM CUENTAS_TB " +
+                    "WHERE CLIENTE_ID = (SELECT CLIENTE_ID FROM CLIENTES_TB WHERE CLIENTE_Identificacion = ?)",
+                    identificacion
+                );
+
+                StringBuilder jsonBuilder = new StringBuilder();
+                jsonBuilder.append("{\"status\":\"OK\", \"cuentas\":[");
+                for (int i = 0; i < cuentas.size(); i++) {
+                    Object[] row = cuentas.get(i);
+                    jsonBuilder.append("{")
+                               .append("\"NumeroCuenta\":\"").append(row[0].toString()).append("\",")
+                               .append("\"Saldo\":").append(row[1])
+                               .append("}");
+                    if (i < cuentas.size() - 1) jsonBuilder.append(",");
+                }
+                jsonBuilder.append("]}");
+                Respuesta = jsonBuilder.toString();
+
+            } else if ("ObtenerMovimientosCuenta".equals(llaveValor.get("TipoDeTransaccion"))) {
+
+                String identificacion = llaveValor.get("Identificacion");
+                String numeroCuenta = llaveValor.get("NumeroCuenta");
+                List<Object[]> movimientos = sqlConnection.selectAll(
+                    "SELECT t.TRAN_FechaHora, " +
+                    "CASE tt.TIPO_TRAN_Nombre " +
+                    "   WHEN 'Retiro' THEN 'Retiro en cajero' " +
+                    "   ELSE tt.TIPO_TRAN_Nombre " +
+                    "END AS Descripcion, " +
+                    "t.TRAN_MontoRetiro AS Monto " +
+                    "FROM TRANSACCIONES_TB t " +
+                    "INNER JOIN CUENTAS_TB c ON t.TRAN_CUENTA_ID = c.CUENTA_ID " +
+                    "INNER JOIN CLIENTES_TB cl ON c.CLIENTE_ID = cl.CLIENTE_ID " +
+                    "INNER JOIN TIPOS_TRANSACCIONES_TB tt ON t.TRAN_TIPO_TRAN_ID = tt.TIPO_TRAN_ID " +
+                    "WHERE cl.CLIENTE_Identificacion = ? AND c.CUENTA_ID = ? " +
+                    "ORDER BY t.TRAN_FechaHora DESC",
+                    identificacion, numeroCuenta
+                );
+
+                StringBuilder jsonBuilder = new StringBuilder();
+                jsonBuilder.append("{\"status\":\"OK\", \"movimientos\":[");
+                for (int i = 0; i < movimientos.size(); i++) {
+                    Object[] row = movimientos.get(i);
+                    jsonBuilder.append("{")
+                               .append("\"Fecha\":\"").append(row[0].toString()).append("\",")
+                               .append("\"Descripcion\":\"").append(row[1].toString()).append("\",")
+                               .append("\"Monto\":").append(row[2])
+                               .append("}");
+                    if (i < movimientos.size() - 1) jsonBuilder.append(",");
+                }
+                
+                jsonBuilder.append("]}");
+                Respuesta = jsonBuilder.toString();
+            
+            //-------------------------------------------------------------------------------------------
+            
             } else{
 
                 throw new Exception();
